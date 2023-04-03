38c5cd5fa2eb478a451de99d8435d7c4e8ee1967
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-04-03 17:32:30 +0200

    add: cards created by javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,70 @@
 const teamMembers = [
   {
+    image: "wayne-barnett-founder-ceo.jpg",
     name: "Wayne Barnett",
-    role: "Founder & CEO",
-    image: "wayne-barnett-founder-ceo.jpg"
+    role: "Founder & CEO"
   },
   {
+    image: "angela-caroll-chief-editor.jpg",
     name: "Angela Caroll",
-    role: "Chief Editor",
-    image: "angela-caroll-chief-editor.jpg"
+    role: "Chief Editor"
   },
   {
+    image: "walter-gordon-office-manager.jpg",
     name: "Walter Gordon",
-    role: "Office Manager",
-    image: "walter-gordon-office-manager.jpg"
+    role: "Office Manager"
   },
   {
+    image: "angela-lopez-social-media-manager.jpg",
     name: "Angela Lopez",
-    role: "Social Media Manager",
-    image: "angela-lopez-social-media-manager.jpg"
+    role: "Social Media Manager"
   },
   {
+    image: "scott-estrada-developer.jpg",
     name: "Scott Estrada",
-    role: "Developer",
-    image: "scott-estrada-developer.jpg"
+    role: "Developer"
   },
   {
+    image: "barbara-ramos-graphic-designer.jpg",
     name: "Barbara Ramos",
-    role: "Graphic designer",
-    image: "barbara-ramos-graphic-designer.jpg"
+    role: "Graphic designer"
   }
 ];
 
-const unorderedList = document.querySelector("ul");
+const row = document.querySelector(".row");
 
 for(let i in teamMembers){
   const teamMember = teamMembers[i];
-  unorderedList.innerHTML +=`
-  <li>Object ${parseInt(i)+1}</li>
-  `
-  const ulGeneral = document.createElement("ul");
-  unorderedList.append(ulGeneral)
+  const col = document.createElement("div");
+  col.classList.add("col", "bg-white", "mb-5", "px-3", "d-flex", "flex-column");
+  row.append(col);
   for(let key in teamMember){
-    if(key === "image"){
-      ulGeneral.innerHTML +=`
-      <li>${key}: <img src="assets/img/${teamMember[key]}"></li>
-      `
-    }else{
-      ulGeneral.innerHTML +=`
-      <li>${key}: ${teamMember[key]}</li>
-      `
+    console.log(key)
+    switch (key) {
+      case "name":
+        const name = document.createElement("h2")
+        name.innerHTML =`
+        ${teamMember[key]}
+        `
+        col.append(name);
+        break;
+        
+      case "role":
+        const role = document.createElement("span")
+        role.innerHTML =`
+        ${teamMember[key]}
+        `
+        col.append(role);
+        break;
+          
+      case "image":
+        const image = document.createElement("img")
+        image.setAttribute("src", "assets/img/" + teamMember[key]);
+        col.append(image);
+        break;
+        
+        default:
+          break;
     }
   }
 }
